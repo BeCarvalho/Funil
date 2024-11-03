3c8dfdd1eb770526afdcb05c0f5f33e292386eac
Ebenézer Carvalho <ebenezercarvalho@gmail.com>
2024-11-02 23:02:38 -0300

    add packages

--- pages/Cyanobacteria_Finder.py
+++ pages/Cyanobacteria_Finder.py
@@ -2,10 +2,14 @@ import streamlit as st
 import subprocess
 import datetime
 import re
+from st_supabase_connection import SupabaseConnection
 
 # Definir as coordenadas geográficas pré-definidas
 LATITUDE = -22.528801960010114
 LONGITUDE = -44.5645781500265
+
+# Inicializar a conexão com o Supabase
+conn = st.connection("supabase", type=SupabaseConnection)
 
 
 # Função para extrair as informações relevantes
@@ -25,6 +29,21 @@ def parse_output(output):
         key, value = line.split(None, 1)
         data[key] = value.strip()
     return data
+
+
+# Função para salvar os dados no Supabase
+def save_to_supabase(data):
+    table = conn.table("CyFi")
+    record = {
+        "criado_em": datetime.datetime.now().isoformat(),
+        "data": data['date'],
+        "latitude": float(data['latitude']),
+        "longitude": float(data['longitude']),
+        "contagem": int(data['density_cells_per_ml'].replace(',', '')),
+        "severidade": data['severity']
+    }
+    response = table.insert(record).execute()
+    return response
 
 
 # Criar o widget de seleção de data
@@ -47,6 +66,16 @@ if st.button("Gerar Previsão"):
             data = parse_output(relevant_info)
             st.success("Previsão gerada com sucesso!")
             st.table(data)
+
+            # Salvar dados no Supabase
+            try:
+                save_response = save_to_supabase(data)
+                if save_response.data:
+                    st.success("Dados salvos com sucesso no Supabase!")
+                else:
+                    st.error("Erro ao salvar dados no Supabase.")
+            except Exception as e:
+                st.error(f"Erro ao salvar dados no Supabase: {str(e)}")
         else:
             st.warning("Não foi possível extrair as informações relevantes da saída.")
     else:
